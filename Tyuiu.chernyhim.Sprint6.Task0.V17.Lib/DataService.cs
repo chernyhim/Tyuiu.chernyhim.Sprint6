98e015df99b285d21f96db91e42ec1b0d3dcb5b4
Tyuiu.chernyhim@outlook.com <Tyuiu.chernyhim@outlook.com>
2025-12-03 15:12:22 +0500

    Add project files.

---
+++ Tyuiu.chernyhim.Sprint6.Task0.V17.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.chernyhim.Sprint6.Task0.V17.Lib
+{
+    public class DataService : ISprint6Task0V17
+    {
+        public double Calculate(int x)
+        {
+            double y = Math.Pow(2.7182, x) / x;
+            y = Math.Round(y, 3);
+            return y;
+        }
+    }
+}
---
+++ Tyuiu.chernyhim.Sprint6.Task0.V17.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.chernyhim.Sprint6.Task0.V17.Lib;
+namespace Tyuiu.chernyhim.Sprint6.Task0.V17.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual(2.718, ds.Calculate(1));
+        }
+    }
+}
