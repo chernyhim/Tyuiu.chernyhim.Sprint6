3b53339cc6f1f32378876fdec21dd05f56a59edb
Tyuiu.chernyhim@outlook.com <Tyuiu.chernyhim@outlook.com>
2025-12-07 13:57:41 +0500

    Task3

---
+++ Tyuiu.chernyhim.Sprint6.Task3.V8.Lib/DataService.cs
@@ -0,0 +1,36 @@
+﻿
+using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.chernyhim.Sprint6.Task3.V8.Lib
+{
+    public class DataService : ISprint6Task3V5
+    {
+        public int[,] Calculate(int[,] matrix)
+        {
+            int[] row = new int[5];
+
+            for (int i = 0; i <= 4; i++)
+            {
+                if (matrix[4,i] % 2 != 0)
+                {
+                    row[i] = matrix[4, i];
+                }
+                else
+                {
+                    row[i] = 0;
+                }
+                
+            }
+
+            
+
+            for (int i = 0; i <= 4; i++)
+            {
+                matrix[4, i] = row[i];
+            }
+
+            return matrix;
+        }
+
+
+    }
+}
---
+++ Tyuiu.chernyhim.Sprint6.Task3.V8.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.chernyhim.Sprint6.Task3.V8.Lib;
+namespace Tyuiu.chernyhim.Sprint6.Task3.V8.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
