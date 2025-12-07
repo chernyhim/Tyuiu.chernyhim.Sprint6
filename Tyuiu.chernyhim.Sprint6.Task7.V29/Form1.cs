17b831e859367e5f2a846b5c54841b3e6d921090
Tyuiu.chernyhim@outlook.com <Tyuiu.chernyhim@outlook.com>
2025-12-07 19:29:39 +0500

    Task7

---
+++ Tyuiu.chernyhim.Sprint6.Task7.V29/Form1.cs
@@ -0,0 +1,17 @@
+using Tyuiu.chernyhim.Sprint6.Task7.V29.Lib;
+namespace Tyuiu.chernyhim.Sprint6.Task7.V29
+{
+    public partial class Form1 : Form
+    {
+        DataService ds = new DataService();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
