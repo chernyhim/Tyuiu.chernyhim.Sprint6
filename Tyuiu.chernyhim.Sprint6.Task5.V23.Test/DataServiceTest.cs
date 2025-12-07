a9b5d6cef257d702f58669a4ae623fa1177866cf
Tyuiu.chernyhim@outlook.com <Tyuiu.chernyhim@outlook.com>
2025-12-07 14:44:36 +0500

    Task5

---
+++ Tyuiu.chernyhim.Sprint6.Task5.V23.Test/DataServiceTest.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.chernyhim.Sprint6.Task5.V23.Lib;
+namespace Tyuiu.chernyhim.Sprint6.Task5.V23.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
---
+++ Tyuiu.chernyhim.Sprint6.Task5.V23/FormMain.cs
@@ -0,0 +1,15 @@
+namespace Tyuiu.chernyhim.Sprint6.Task5.V23
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
