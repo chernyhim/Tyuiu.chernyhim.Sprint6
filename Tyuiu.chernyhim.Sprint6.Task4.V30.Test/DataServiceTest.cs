14f87e94ffae15a8f66e4ce43e32cc2fcf1d088f
Tyuiu.chernyhim@outlook.com <Tyuiu.chernyhim@outlook.com>
2025-12-07 14:28:25 +0500

    Task4

---
+++ Tyuiu.chernyhim.Sprint6.Task4.V30.Test/DataServiceTest.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.chernyhim.Sprint6.Task4.V30.Lib;
+namespace Tyuiu.chernyhim.Sprint6.Task4.V30.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
---
+++ Tyuiu.chernyhim.Sprint6.Task4.V30/FormMain.cs
@@ -0,0 +1,15 @@
+namespace Tyuiu.chernyhim.Sprint6.Task4.V30
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
