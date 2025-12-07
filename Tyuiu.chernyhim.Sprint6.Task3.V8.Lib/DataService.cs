d758129058dd382c4e0a484646fb11fcf2bdd262
Tyuiu.chernyhim@outlook.com <Tyuiu.chernyhim@outlook.com>
2025-12-07 14:10:46 +0500

    Task3

--- Tyuiu.chernyhim.Sprint6.Task3.V8.Lib/DataService.cs
+++ Tyuiu.chernyhim.Sprint6.Task3.V8.Lib/DataService.cs
@@ -5,25 +5,9 @@ namespace Tyuiu.chernyhim.Sprint6.Task3.V8.Lib
     {
         public int[,] Calculate(int[,] matrix)
         {
-            int rows = matrix.GetUpperBound(0) + 1;
-            int columns = matrix.Length / rows;
-            int[,] mat = new int[rows, columns];
 
-            for (int i = 0; i < rows; i++)
-            {
-                for (int j = 0; j < columns; j++)
-                {
-                    if (matrix[i, j] % 2 == 0 && i == 3)
-                    {
-                        mat[i, j] = 0;
-                    }
-                    else
-                    {
-                        mat[i,j]=matrix[i,j];
-                    }
-                }
-            }
-            return mat;
+
+            return matrix;
         }
     }
 }
