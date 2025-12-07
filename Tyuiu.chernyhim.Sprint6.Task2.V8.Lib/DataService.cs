1e484b6fbdc35e6210985e23ae8d003d2312d01b
Tyuiu.chernyhim@outlook.com <Tyuiu.chernyhim@outlook.com>
2025-12-07 13:32:28 +0500

    Task2

---
+++ Tyuiu.chernyhim.Sprint6.Task2.V8.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.chernyhim.Sprint6.Task2.V8.Lib
+{
+    public class DataService : ISprint6Task2V8
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] valueArray;
+            int len = (stopValue - startValue) + 1;
+            valueArray = new double[len];
+            double y;
+            int count = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                y = Math.Round(Math.Cos(2 * x) + (Math.Sin(x) / (x + 2.5)) + 2 * x, 2);
+                valueArray[count] = y;
+                count++;
+            }
+            return valueArray;
+        }
+    }
+}
---
+++ Tyuiu.chernyhim.Sprint6.Task2.V8/FormMain.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.chernyhim.Sprint6.Task2.V8
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
